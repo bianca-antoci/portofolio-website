d5ca05cad60ad422ecf1cf5674a1dfbc3110e370
Bianca Antoci <bianca.antoci96@gmail.com>
2021-02-06 12:23:23 +0000

    add menu bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+
+
+
 
 @NgModule({
   declarations: [
@@ -10,7 +19,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+    MatToolbarModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
